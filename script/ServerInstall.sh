60912df74bc8853c2f303514aef6ceaee8358061
Ciliste <95741289+Ciliste@users.noreply.github.com>
2023-12-11 14:31:48 +0100

    Update ServerInstall.sh

--- script/ServerInstall.sh
+++ script/ServerInstall.sh
@@ -3,3 +3,7 @@ if [ -d "./TronStadium" ]; then
 	echo "TronStadium directory exists, deleting it..."
 	rm -fR ./TronStadium
 fi
+
+# Clone the TronStadium repository
+echo "Cloning TronStadium repository..."
+git clone https://github.com/Ciliste/TronStadium.git
