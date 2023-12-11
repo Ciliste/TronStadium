cee029d65c0ac18d4d173a96f07b8fd6f9401f8f
Ciliste <95741289+Ciliste@users.noreply.github.com>
2023-12-11 14:38:32 +0100

    Update ServerInstall.sh

--- script/ServerInstall.sh
+++ script/ServerInstall.sh
@@ -7,3 +7,12 @@ fi
 # Clone the TronStadium repository
 echo "Cloning TronStadium repository..."
 git clone https://github.com/Ciliste/TronStadium.git
+
+# If database directory exists, then delete it
+if [ -d "./database" ]; then
+	echo "Database directory exists, deleting it..."
+	rm -fR ./database
+fi
+
+echo "Moving database directory..."
+mv ./TronStadium/server/database ./
