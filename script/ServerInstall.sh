33f79d3a2f7f9d3d737feff8ce0c4e1350568135
Ciliste <95741289+Ciliste@users.noreply.github.com>
2023-12-11 15:00:05 +0100

    Update ServerInstall.sh

--- script/ServerInstall.sh
+++ script/ServerInstall.sh
@@ -18,6 +18,12 @@ fi
 echo "Moving database directory..."
 mv ./TronStadium/server/database ./
 
+# If scripts files exists, then delete it
+if [ -f "./ServerStart.sh" ]; then
+	echo "ServerStart.sh file exists, deleting it..."
+	rm -f ./ServerStart.sh
+fi
+
 # Move scripts
 echo "Moving start script..."
 mv ./TronStadium/script/ServerStart.sh ./
