0c578994d1bba05b1be94c115f56f4c82c44a958
Ciliste <95741289+Ciliste@users.noreply.github.com>
2023-12-11 14:29:37 +0100

    Update ServerInstall.sh

--- script/ServerInstall.sh
+++ script/ServerInstall.sh
@@ -1,5 +1,5 @@
 # If TronStadium directory exists, then delete it
 if [ -d "./TronStadium" ]; then
 	echo "TronStadium directory exists, deleting it..."
-	rm -r ./TronStadium
+	rm -fR ./TronStadium
 fi
