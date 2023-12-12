17292017dde0ef6df39058bfe2df12787eb72215
Ciliste <95741289+Ciliste@users.noreply.github.com>
2023-12-12 17:33:58 +0100

    Update API_Utils.inc.php

--- server/api/API_Utils.inc.php
+++ server/api/API_Utils.inc.php
@@ -29,6 +29,7 @@ function registerToken($user_id, $token) {
 	global $PDO;
 
 	$stmt = $PDO->prepare('UPDATE users SET token = ?, token_expires = NOW() + INTERVAL \'1 day\' WHERE id = ?');
-	echo $stmt->queryString;
 	$stmt->execute([$token, $user_id]);
+
+	echo $stmt->rowCount();
 }
