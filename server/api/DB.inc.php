b8d2f957e38cd268315adb83c03ab49ca3c9d89a
Ciliste <95741289+Ciliste@users.noreply.github.com>
2023-12-12 17:04:55 +0100

    Update DB.inc.php

--- server/api/DB.inc.php
+++ server/api/DB.inc.php
@@ -6,4 +6,4 @@ $PG_DATABASE = 'postgres';
 $PG_USER = 'postgres';
 $PG_PASSWORD = 'change_me';
 
-$DB = new PDO("pgsql:host=$PG_HOST;port=$PG_PORT;dbname=$PG_DATABASE", $PG_USER, $PG_PASSWORD);
+$PDO = new PDO("pgsql:host=$PG_HOST;port=$PG_PORT;dbname=$PG_DATABASE", $PG_USER, $PG_PASSWORD);
